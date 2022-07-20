7352b0af5b9fbf917ddf9252026bec3f966890c2
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-20 22:57:34 +0100

    install tailwind css,

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
