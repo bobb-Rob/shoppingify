1dc40dddf2a3a5be953c10a3657826ee5131bbcf
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-31 23:54:51 +0100

    add color to shoppingify text in header

--- src/components/List/Header.js
+++ src/components/List/Header.js
@@ -6,12 +6,13 @@ const Header = () => {
 
   return (
     <header className="flex items-center">
-      <span>
-        Shoppingify allows you take your
+      <span className="">
+        <span className="text-orange">Shoppingify </span>
+        allows you take your
         <br />
         shopping list wherever you go
       </span>
-      <div className="flex items-center h-10 p-4 rounded-md shadow-lg">
+      <div className="flex items-center h-10 p-4 rounded-md shadow-xl">
         <BiSearchAlt2 size={22} />
         <input
           type="text"
