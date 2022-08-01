615a0329271aec35e34a8563a997adc6a49bdd98
Robertson Arthur <robertsonakpan@gmail.com>
2022-08-02 00:53:57 +0100

    styled item section top

--- src/components/itemSection.js
+++ src/components/itemSection.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Button from './Button';
 import ShoppingList from './ShoppingList';
+import sourceBottle from '../assets/source.svg';
 
 const ItemSection = () => (
-  <section className="Items border">
-    <div className="list-top">
-      <div>
-        <img src="" alt="" />
+  <section className="Items bg-peach px-12 py-10">
+    <div className="list-top text-white flex bg-[#80485B] h-[129.91px] rounded-2xl">
+      <div className="relative w-[78.67px]">
+        <img
+          className="absolute -top-4 left-2"
+          src={sourceBottle}
+          alt="source"
+        />
       </div>
-      <div>
+      <div className="py-4 pl-9 font-bold">
         <p>Didn&#39;t find what you need?</p>
-        <Button name="Add item" />
+        <Button
+          klassName="bg-white text-black rounded-xl mt-2"
+          name="Add item"
+        />
       </div>
     </div>
     <div className="shopping-list">
