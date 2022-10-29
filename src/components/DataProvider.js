49936cad4b593d3f248d6ad74c30d9a5f8fb52c5
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-29 16:32:22 +0100

    Implement from end validation using React-hook-form

--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -1,43 +1,30 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext } from 'react';
 import { useDispatch } from 'react-redux';
-import { registerUser } from '../redux/RSidebarReducers/user/userSlice';
+// import { registerUser } from '../redux/RSidebarReducers/user/userSlice';
 
 export const UserContext = createContext();
 
 const DataProvider = ({children}) => {
-  const dispatch = useDispatch();
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    const user1 = {
-      ...user,
-      [name]: value,
-    };
-    setUser(user1);
-  };
-
-  const onSubmit = (e) => {
+  const dispatch = useDispatch(); 
+  
+  const onSubmit = (user, e) => {
     e.preventDefault();
-    dispatch(registerUser(user)).then((response) => {
-      const { code } = response.payload.status;
-      if (code === 200) {
-        setUser({
-          name: '',
-          email: '',
-          password: '',
-        });
-      }
-    });
+    console.log(user)
+    // dispatch(registerUser(user)).then((response) => {
+    //   const { code } = response.payload.status;
+    //   if (code === 200) {
+    //     setUser({
+    //       name: '',
+    //       email: '',
+    //       password: '',
+    //     });
+    //   }
+    // });
   };
 
   return (
     <UserContext.Provider value={{
-      dispatch, handleChange, onSubmit, user
+      dispatch, onSubmit
     }}>
       {children}
     </UserContext.Provider>
--- src/components/user/Registration.js
+++ src/components/user/Registration.js
@@ -1,37 +1,79 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../DataProvider';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { HiOutlineUser, HiOutlineMail, HiOutlineArrowSmRight } from 'react-icons/hi';
+import { RiUserLine } from 'react-icons/ri';
 import { MdOutlineLock } from 'react-icons/md';
 import BackArrow from '../reusables/BackArrow';
 
 const Registration = () => {
-  const { handleChange, onSubmit, user } = useContext(UserContext);
-  
+  const { onSubmit } = useContext(UserContext);
+  const { register, handleSubmit, formState: {errors} } = useForm({
+    defaultValues: {
+      username: '',
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+    }
+  });
+
   return (
     <div className="user-container flex flex-col space-between">
       <BackArrow classNam={"register-back-btn"} />
       <div className="user-form-wrap flex flex-col">
         <h2>Create Account</h2>
-        <form onSubmit={onSubmit}>
-          <div className="relative mb-4 mt-6 border-bottom">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="relative mb-4">
             <label
-              htmlFor="name"
+              htmlFor="username"
               className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
             >
               <HiOutlineUser />
             </label>           
             <input
               type="text"
-              name="name"
-              id="name"
-              value={user.name}
-              onChange={handleChange}
-              className="w-full pl-10 p-2 rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
-              placeholder='Full name'
+              id="username"
+              {...register('username', {required: "Username must be provided" })}             
+              className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
+              placeholder='Username'
             />
+            <p className='invalid mb-2'>{errors.username?.message}</p>
           </div>
-          <div className="relative mt-6 mb-4 border-bottom">            
+          <div className="relative mb-4">
+            <label
+              htmlFor="firstName"
+              className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
+            >
+              <RiUserLine />
+            </label>           
+            <input
+              type="text"
+              id="firstName"
+              {...register('first_name', {required: "First name must be provided" })}             
+              className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
+              placeholder='First name'
+            />
+            <p className='invalid mb-2'>{errors.first_name?.message}</p>
+          </div>
+          <div className="relative mb-4 ">
+            <label
+              htmlFor="lastName"
+              className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
+            >
+              <RiUserLine />
+            </label>           
+            <input
+              type="text"
+              id="lastName"
+              {...register('last_name', {required: "Last must be provided" })}             
+              className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
+              placeholder='Last name'
+            />
+            <p className='invalid mb-2'>{errors.last_name?.message}</p>
+          </div>
+          <div className="relative mb-4 ">            
             <label
               htmlFor="email"
               className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
@@ -39,16 +81,15 @@ const Registration = () => {
               <HiOutlineMail />
             </label>           
             <input
-              type="email"
-              name="email"
+              type="email"              
               id="email"
-              value={user.email}
-              onChange={handleChange}
+              {...register('email', {required: "Email must be provided" })}              
               placeholder='Email'
-              className="w-full pl-10 p-2 rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
+              className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
             />
+            <p className='invalid mb-2'>{errors.email?.message}</p>
           </div>
-          <div className="relative mt-6 mb-4 border-bottom">
+          <div className="relative mt-6 mb-4 ">
             <label
               htmlFor="password"
               className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
@@ -56,14 +97,16 @@ const Registration = () => {
               <MdOutlineLock />
             </label>   
             <input
-              type="password"
-              name="password"
+              type="password"              
               id="password"
-              value={user.password}
-              onChange={handleChange}
+              {...register('password', { 
+                required: "Password must be provided",
+                minLength: {value: 6, message: "Password must be at least 6 characters long"}
+              })}         
               placeholder='Password'
-              className="w-full pl-10 p-2 rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
+              className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
             />
+            <p className='invalid mb-2'>{errors.password?.message}</p>
           </div>
           <button
             type="submit"
