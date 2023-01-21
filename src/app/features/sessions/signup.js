243abef29627dc877eecfded52a05b4b707988f0
Robertson Akpan <robertsonakpan@gmail.com>
2023-01-21 21:40:06 +0100

    Resize sign up page

--- src/app/features/sessions/signup.js
+++ src/app/features/sessions/signup.js
@@ -56,107 +56,109 @@ const SignUp = () => {
   }
 
   return (
-    <div className="user-container flex flex-col space-between">
-      <div className="user-form-wrap flex flex-col">
-        <div>{allErrors}</div>
-        <h2>Sign up</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="relative mt-6 mb-4 ">
-            <label
-              htmlFor="firstName"
-              className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
+    <section>
+      <div className="user-container w-5/6 md:flex md:max-w-3xl mx-auto mt-10 md:mt-20 shadow-lg">
+        <div className="user-form-wrap flex flex-col">
+          <div>{allErrors}</div>
+          <h2>Sign up</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="relative mt-6 mb-4 ">
+              <label
+                htmlFor="firstName"
+                className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
+              >
+                <MdOutlineLock />
+              </label>
+              <input
+                type="text"
+                id="firstName"
+                {...register('first_name', {
+                  required: 'First name is required',
+                  minLength: {
+                    value: 2,
+                    message: 'first name must be at least 2 characters long',
+                  },
+                })}
+                placeholder="First Name"
+                className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
+              />
+              <p className="invalid mb-2">{errors.first_name?.message}</p>
+            </div>
+            <div className="relative mt-6 mb-4 ">
+              <label
+                htmlFor="lastName"
+                className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
+              >
+                <MdOutlineLock />
+              </label>
+              <input
+                type="text"
+                id="lastName"
+                {...register('last_name', {
+                  required: true,
+                })}
+                placeholder="Last Name"
+                className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
+              />
+              <p className="invalid mb-2">{errors.password?.message}</p>
+            </div>
+            <div className="relative mb-4 ">
+              <label
+                htmlFor="email"
+                className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
+              >
+                <HiOutlineMail />
+              </label>
+              <input
+                type="email"
+                id="email"
+                {...register('email', { required: 'Email must be provided' })}
+                placeholder="Email"
+                className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
+              />
+              <p className="invalid mb-2">{errors.email?.message}</p>
+            </div>
+            <div className="relative mt-6 mb-4 ">
+              <label
+                htmlFor="password"
+                className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
+              >
+                <MdOutlineLock />
+              </label>
+              <input
+                type="password"
+                id="password"
+                {...register('password', {
+                  required: 'Password must be provided',
+                  minLength: {
+                    value: 6,
+                    message: 'Password must be at least 6 characters long',
+                  },
+                })}
+                placeholder="Password"
+                className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
+              />
+              <p className="invalid mb-2">{errors.password?.message}</p>
+            </div>
+            <button
+              type="submit"
+              className="self-end bg-gradient-to-r from-[#f7c659] to-[#fda33c] text-white w-[120px] py-2 px-2 rounded-3xl mt-6 flex items-center justify-center"
             >
-              <MdOutlineLock />
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              {...register('first_name', {
-                required: 'First name is required',
-                minLength: {
-                  value: 2,
-                  message: 'first name must be at least 2 characters long',
-                },
-              })}
-              placeholder="First Name"
-              className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
-            />
-            <p className="invalid mb-2">{errors.first_name?.message}</p>
-          </div>
-          <div className="relative mt-6 mb-4 ">
-            <label
-              htmlFor="lastName"
-              className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
-            >
-              <MdOutlineLock />
-            </label>
-            <input
-              type="text"
-              id="lastName"
-              {...register('last_name', {
-                required: true,
-              })}
-              placeholder="Last Name"
-              className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
-            />
-            <p className="invalid mb-2">{errors.password?.message}</p>
-          </div>
-          <div className="relative mb-4 ">
-            <label
-              htmlFor="email"
-              className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
-            >
-              <HiOutlineMail />
-            </label>
-            <input
-              type="email"
-              id="email"
-              {...register('email', { required: 'Email must be provided' })}
-              placeholder="Email"
-              className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
-            />
-            <p className="invalid mb-2">{errors.email?.message}</p>
-          </div>
-          <div className="relative mt-6 mb-4 ">
-            <label
-              htmlFor="password"
-              className="flex items-center z-10 gap-x-2 text-xl absolute px-3 py-2"
-            >
-              <MdOutlineLock />
-            </label>
-            <input
-              type="password"
-              id="password"
-              {...register('password', {
-                required: 'Password must be provided',
-                minLength: {
-                  value: 6,
-                  message: 'Password must be at least 6 characters long',
-                },
-              })}
-              placeholder="Password"
-              className="w-full pl-10 p-2 border-bottom rounded-md focus:outline-none focus:drop-shadow-lg sm:text-sm"
-            />
-            <p className="invalid mb-2">{errors.password?.message}</p>
-          </div>
-          <button
-            type="submit"
-            className="self-end bg-gradient-to-r from-[#f7c659] to-[#fda33c] text-white w-[120px] py-2 px-2 rounded-3xl mt-6 flex items-center justify-center"
-          >
-            <span className="text-md">Create Account</span>
-            <HiOutlineArrowSmRight className="inline-block ml-2 text-xl" />
-          </button>
-        </form>
+              <span className="text-md">Create Account</span>
+              <HiOutlineArrowSmRight className="inline-block ml-2 text-xl" />
+            </button>
+          </form>
+        </div>
+        <div>
+          <span>
+            Already have an account?
+            <Link to="/login" className="">
+              Sign in
+            </Link>
+          </span>
+        </div>
       </div>
-      <div>
-        <span>
-          Already have an account?
-          <Link to="/login" className="">
-            Sign in
-          </Link>
-        </span>
-      </div>
-    </div>
+    </section>
   );
 };
 
