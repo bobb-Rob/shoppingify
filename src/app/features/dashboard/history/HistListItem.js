bf304d0593825e35e65f0ca88d5b893ebf6d3357
Robertson Akpan <robertsonakpan@gmail.com>
2023-02-01 03:09:15 +0100

    Shopping List History Componet List items completed

--- src/app/features/dashboard/history/HistListItem.js
+++ src/app/features/dashboard/history/HistListItem.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FcNext } from 'react-icons/fc';
+
+import { HiOutlineCalendar } from 'react-icons/hi';
+import { MdNavigateNext } from 'react-icons/md';
 
 const HistListItem = ({ listName, date, status }) => (
-  <div className="flex justify-between">
-    <h3>{listName}</h3>
-    <div className="flex gap">
-      <span>{date}</span>
-      <div>{status}</div>
-      <FcNext />
+  <div className="flex justify-between items-center cursor-pointer shadow-[0_2px_8px_-1px_rgba(181,181,181,1)] rounded-xl p-4 pr-2 mt-4">
+    <h3
+      className="font-medium leading-[20px] "
+    >
+      {listName}
+    </h3>
+    <div className="flex items-center">
+      <div className="flex items-center gap-2 text-xs mr-4 text-gray-400">
+        <HiOutlineCalendar />
+        <span>{date}</span>
+      </div>
+      <div
+        className={
+          status === 'complete'
+            ? 'border border-blue text-blue text-xs w-[90px] px-2 py-1 rounded-xl flex justify-center'
+            : 'border border-myRed text-myRed text-xs w-[90px] px-2 py-1 rounded-xl flex justify-center'
+        }
+      >
+        {status === 'complete' ? 'Completed' : 'Cancelled'}
+      </div>
+      <MdNavigateNext className="text-orange ml-2 text-2xl cursor-pointer" />
     </div>
   </div>
 );
